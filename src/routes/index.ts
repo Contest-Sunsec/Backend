563c92a55ab95f0d6e47c6c037e0dcfdfd2dcebb
PleBea <bini060315kim@gmail.com>
2022-09-24 04:10:12 +0900

    add test route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import authRouter from './auth';
 
 const router = Router();
 
 router.use('/auth', authRouter);
+
+router.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'Hello World!' });
+});
 
 export default router;
