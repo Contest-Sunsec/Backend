92631a5b90a10c1a12c557265acbe14d7735dba7
PleBea <bini060315kim@gmail.com>
2022-09-24 04:10:26 +0900

    change syntax

--- src/validation/auth.ts
+++ src/validation/auth.ts
@@ -1,13 +1,9 @@
 const validateEmail = (email: string) => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 };
 
-const validatePassword = (password: string) => {
-    if (password.length < 6) {
-        return false;
-    }
-    return true;
-};
+const validatePassword = (password: string) => password.length >= 6;
 
 export { validateEmail, validatePassword };
